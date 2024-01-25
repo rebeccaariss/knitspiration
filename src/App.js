9963daac7dc66580c2d230bdeb4198b81e24d2d0
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-01-24 23:21:26 -0500

    Set up basic Unsplash API call for testing ImageList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { ImageList } from '@mui/material';
 
-function App() {
+function App({ photosArray }) {
   return (
     <div className="App">
       <h1>Welcome to knitspiration!</h1>
+      <ImageList />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let photosArray = [];
+
+// API documentation for HTTP Authorization header method (see "headers," below):
+// https://unsplash.com/documentation#authorization
+
+let count = 5;
+const apiUrl = 'https://api.unsplash.com/photos/random/';
+const headers = {
+  Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`,
+};
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl + `?count=${count}`, { headers });
+    photosArray = await response.json();
+  } catch (error) {
+    // Error handling here
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App photosArray={photosArray} />
   </React.StrictMode>
 );
+
+getPhotos();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
