91d3dcecf13ac8befd3561b3070825c5c0b1d79b
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-01-24 23:43:18 -0500

    Move API call into App.js; implement async call with useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import { ImageList } from '@mui/material';
+import { ImageList, ImageListItem } from '@mui/material';
 
-function App({ photosArray }) {
+// API documentation for HTTP Authorization header method (see "headers," below):
+// https://unsplash.com/documentation#authorization
+
+let count = 5;
+const apiUrl = 'https://api.unsplash.com/photos/random/';
+const headers = {
+  Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`,
+};
+
+function App() {
+  const [photosArray, setPhotosArray] = useState([]);
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      try {
+        const response = await fetch(apiUrl + `?count=${count}`, { headers });
+        const data = await response.json();
+        setPhotosArray(data);
+      } catch (error) {
+        console.error('Error fetching photos:', error);
+      }
+    }
+    fetchPhotos();
+  }, []); // square brackets necessary to prevent errors
+
   return (
     <div className="App">
       <h1>Welcome to knitspiration!</h1>
-      <ImageList />
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {photosArray.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.urls.regular}?w=248&fit=crop&auto=format`}
+              alt={item.alt_description || ''}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
